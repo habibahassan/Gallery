861c62c9784f3b99df4ab1a4f28bde407e214b15
habibahassan <halimaadan92@gmail.com>
2020-02-29 16:55:21 +0300

    updated views.py and home.html

--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,5 @@
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 def homePageView(request):
- return HttpResponse('Hello, World!')
+  return render(request, 'home.html')
