601ed6450ad3d45282602d8cc3a71a6ea9f94c33
habibahassan <halimaadan92@gmail.com>
2020-02-29 22:31:40 +0300

    created category models on model.py

--- photos/models.py
+++ photos/models.py
@@ -14,14 +14,14 @@ class Location(models.Model):
     def save_location(self):
         self.save()
 
-# class Category(models.Model):
-#     category = models.CharField(max_length =30)
+class Category(models.Model):
+    category = models.CharField(max_length =30)
 
-#     def __str__(self):
-#         return self.category
+    def __str__(self):
+        return self.category
     
-#     def save_category(self):
-#         self.save()
+    def save_category(self):
+        self.save()
 
 # class Image(models.Model):
 #     image_name = models.CharField(max_length =60)
