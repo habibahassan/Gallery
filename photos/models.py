46451855f8b6503442974f1f70c9e6aed8ab9bf2
habibahassan <halimaadan92@gmail.com>
2020-02-29 22:32:23 +0300

    created images models on model.py and on admin.py

--- photos/models.py
+++ photos/models.py
@@ -23,46 +23,46 @@ class Category(models.Model):
     def save_category(self):
         self.save()
 
-# class Image(models.Model):
-#     image_name = models.CharField(max_length =60)
-#     image_description = models.CharField(max_length =255)
-#     category = models.ManyToManyField(Category)
-#     location = models.ForeignKey('Location', on_delete=models.CASCADE,)
-#     pub_date = models.DateTimeField(auto_now_add=True)
-#     image = models.ImageField(upload_to='images/',default='DEFAULT VALUE')
+class Image(models.Model):
+    image_name = models.CharField(max_length =60)
+    image_description = models.CharField(max_length =255)
+    category = models.ManyToManyField(Category)
+    location = models.ForeignKey('Location', on_delete=models.CASCADE,)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images/',default='DEFAULT VALUE')
     
-#     def __str__(self):
-#         return self.image_name
+    def __str__(self):
+        return self.image_name
     
-#     def save_image(self):
-#         self.save()
+    def save_image(self):
+        self.save()
     
-#     @classmethod
-#     def get_all_images(cls):
-#         images = cls.objects.all()
-#         return images
+    @classmethod
+    def get_all_images(cls):
+        images = cls.objects.all()
+        return images
     
-#     @classmethod
-#     def get_image_by_id(cls,id):
-#         image = cls.objects.filter(id=id)
-#         return image
+    @classmethod
+    def get_image_by_id(cls,id):
+        image = cls.objects.filter(id=id)
+        return image
     
-#     @classmethod
-#     def delete_image_by_id(cls,id):
-#         image = cls.objects.remove(id=id)
-#         return image
+    @classmethod
+    def delete_image_by_id(cls,id):
+        image = cls.objects.remove(id=id)
+        return image
     
-#     @classmethod
-#     def update_image_by_id(cls,id):
-#         image = cls.objects.update(id=id)
-#         return image
+    @classmethod
+    def update_image_by_id(cls,id):
+        image = cls.objects.update(id=id)
+        return image
     
-#     @classmethod
-#     def search_by_location(cls,search_term):
-#         image = Image.objects.filter(location__id=search_term).all()
-#         return image
+    @classmethod
+    def search_by_location(cls,search_term):
+        image = Image.objects.filter(location__id=search_term).all()
+        return image
     
-#     @classmethod
-#     def search_by_category(cls,search_term):
-#         image = Image.objects.filter(category__icontains=search_term).all()
-#         return image
+    @classmethod
+    def search_by_category(cls,search_term):
+        image = Image.objects.filter(category__icontains=search_term).all()
+        return image
