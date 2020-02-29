10172f9608c71482f1307c583bf16bc045db8bb3
habibahassan <halimaadan92@gmail.com>
2020-02-29 21:59:57 +0300

    added postgres to the priject

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    location = models.CharField(max_length =30)
+    
+
+    def __str__(self):
+        return self.location
+    
+    class Meta:
+        ordering = ['location']
+        
+    def save_location(self):
+        self.save()
